d0c18a492e31ec28cd62a0d3916b96201222d154
Nikola Najic <nnajic1@gmail.com>
2024-03-28 10:31:13 +0100

    Napravljene klase Vozilo, Put, Raskrsnica i podklasa Garaza (#4, #6, #7)

--- Classes.py
+++ Classes.py
@@ -1,7 +1,8 @@
+import random
 
 class Vozilo:
 
-    def __init__(self, tip : str, id : int) -> None:
+    def __init__(self, tip : str, id : str) -> None:
         self._tip = tip
         self._id = id
         self._duzina = None
@@ -12,16 +13,16 @@ class Vozilo:
         else:
             raise Exception("Nedefinisan tip vozila!")
     
-    def Uzmi_duzinu(self):
+    def Uzmi_duzinu(self) -> int:
         return self._duzina
     
-    def Uzmi_id(self):
+    def Uzmi_id(self) -> str:
         return self._id
     
-    def Uzmi_brzinu(self):
+    def Uzmi_brzinu(self) -> int:
         return self._brzina
     
-    def Uzmi_tip(self):
+    def Uzmi_tip(self) -> str:
         return self._tip
     
 class Put:
@@ -68,3 +69,37 @@ class Put:
     def Dodaj_vozilo(self, vozilo : Vozilo) -> None:    # Mora da se pozove iskljucivo nakon provere da li ima prostora na ulici za dato vozilo !!
         self.prostor_na_ulici[:vozilo.Uzmi_duzinu()] = vozilo
 
+class Raskrsnica:
+
+    def __init__(self, id : int, koordinate : tuple[int, int]) -> None:
+        self._id = id
+        self._ulazni_putevi = []
+        self._izlazni_putevi = []
+        self._koordinate = koordinate
+
+
+class Garaza(Raskrsnica):
+
+    dostupni_tipovi_vozila = ['auto']
+
+    def __init__(self, id: int, koordinate: tuple[int, int], vreme_stvaranja : float, id_raskrsnice : int) -> None:
+        super().__init__(id, koordinate)
+        self._vreme_stvaranja = vreme_stvaranja
+        self._izlazni_putevi = [id_raskrsnice]
+        self._napravljenih_vozila = 1
+    
+    def Uzmi_id(self) -> int:
+        return self._id
+
+    def Uzmi_broj_vozila(self) -> int:
+        return self._napravljenih_vozila
+    
+    def Uvecaj_broj_vozila(self) -> None:
+        self._napravljenih_vozila += 1
+    
+    def Stvori_vozilo(self):
+        tip = random.choice(Garaza.dostupni_tipovi_vozila)
+        id = self.Uzmi_id() + "_" + self.Uzmi_broj_vozila()
+        self.Uvecaj_broj_vozila()
+        vozilo = Vozilo(tip, id)
+        return vozilo
