ceff9b3cdd31ccbf3b84bcd2cc0d5d8902afea7c
Nikola Najic <nnajic1@gmail.com>
2024-03-28 10:07:00 +0100

    Promena formata naziva funkcija

--- Classes.py
+++ Classes.py
@@ -1,49 +1,4 @@
 
-class Put:
-    
-    def __init__(self, duzina : int, max_brzina : int, id : int, id_raskrsnica : tuple[int, int], semafor : Prepreka) -> None:
-        self._duzina = duzina
-        self._max_brzina = max_brzina
-        self._id = id
-        self._id_raskrsnica = id_raskrsnica
-        self._semafor = semafor
-        self.prostor_na_ulici = [None for _ in range(duzina)]
-        self._slobodno_mesto = duzina
-    
-    def UzmiId(self):
-        return self._id
-
-    def UzmiMaxBrzinu(self):
-        return self._max_brzina
-
-    def UzmiRaskrsnice(self):
-        return self._id_raskrsnica
-
-    def ProveriSemafor(self):
-        return self._semafor._stanje
-    
-    def AzurirajSemafor(self):
-        self._semafor = self._semafor.Promeni_stanje()
-    
-    def ImaMesta(self):
-        return self._slobodno_mesto
-
-    def PomeriVozila(self):
-        self.AzurirajSemafor()
-        if self.ProveriSemafor():
-            vozilo = self.prostor_na_ulici[-1]
-            self.prostor_na_ulici = [None] + self.prostor_na_ulici[:-1]
-            return vozilo
-        else:
-            for i in range(self._duzina - 1, 0, -1):
-                if self.prostor_na_ulici[i] == None:
-                    self.prostor_na_ulici[:i] = [None] + self.prostor_na_ulici[:i - 1]
-                    return
-    
-    def DodajVozilo(self, vozilo : Vozilo): # Mora da se pozove iskljucivo nakon provere da li ima prostora na ulici za dato vozilo !!
-        self.prostor_na_ulici[:vozilo.UzmiDuzinu()] = vozilo
-
-
 class Vozilo:
 
     def __init__(self, tip : str, id : int) -> None:
@@ -57,14 +12,59 @@ class Vozilo:
         else:
             raise Exception("Nedefinisan tip vozila!")
     
-    def UzmiDuzinu(self):
+    def Uzmi_duzinu(self):
         return self._duzina
     
-    def UzmiId(self):
+    def Uzmi_id(self):
         return self._id
     
-    def UzmiBrzinu(self):
+    def Uzmi_brzinu(self):
         return self._brzina
     
-    def UzmiTip(self):
+    def Uzmi_tip(self):
         return self._tip
+    
+class Put:
+    
+    def __init__(self, duzina : int, max_brzina : int, id : int, id_raskrsnica : tuple[int, int], semafor) -> None:
+        self._duzina = duzina
+        self._max_brzina = max_brzina
+        self._id = id
+        self._id_raskrsnica = id_raskrsnica
+        self._semafor = semafor
+        self.prostor_na_ulici = [None for _ in range(duzina)]
+        self._slobodno_mesto = duzina
+    
+    def Uzmi_id(self) -> int:
+        return self._id
+
+    def Uzmi_max_brzinu(self) -> int:
+        return self._max_brzina
+
+    def Uzmi_raskrsnice(self) -> tuple[int, int]:
+        return self._id_raskrsnica
+
+    def Proveri_semafor(self):
+        return self._semafor._stanje
+    
+    def Azuriraj_semafor(self) -> None:
+        self._semafor = self._semafor.Promeni_stanje()
+    
+    def Ima_mesta(self) -> int:
+        return self._slobodno_mesto
+
+    def Pomeri_vozila(self) -> Vozilo:
+        self.Azuriraj_semafor()
+        if self.Proveri_semafor():
+            vozilo = self.prostor_na_ulici[-1]
+            self.prostor_na_ulici = [None] + self.prostor_na_ulici[:-1]
+            return vozilo
+        else:
+            for i in range(self._duzina - 1, 0, -1):
+                if self.prostor_na_ulici[i] == None:
+                    self.prostor_na_ulici[:i] = [None] + self.prostor_na_ulici[:i - 1]
+                    return
+    
+    def Dodaj_vozilo(self, vozilo : Vozilo) -> None:    # Mora da se pozove iskljucivo nakon provere da li ima prostora na ulici za dato vozilo !!
+        self.prostor_na_ulici[:vozilo.Uzmi_duzinu()] = vozilo
+
